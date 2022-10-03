65aa7e48299d12ec0b066f35acf49ecfc77b2883
kisb2 <kisb1024@gmail.com>
2022-10-03 15:03:22 +0200

    initial commit

---
+++ Projects/Joke_teller/joketeller.py
@@ -0,0 +1,8 @@
+telljoke = True
+
+while telljoke:
+    ans = str(input('Do you want to read a joke?(y => yes; n => exit): '))
+    if(ans == 'y'):
+        print('joke')
+    else:
+        telljoke = False
