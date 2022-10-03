79b3829b9bf5f6fd92d7a827ab83c986692c9e59
kisb2 <kisb1024@gmail.com>
2022-10-03 15:10:34 +0200

    adding joke fetching

--- Projects/Joke_teller/joketeller.py
+++ Projects/Joke_teller/joketeller.py
@@ -1,8 +1,20 @@
-telljoke = True
+import requests
+import time
 
-while telljoke:
-    ans = str(input('Do you want to read a joke?(y => yes; n => exit): '))
-    if(ans == 'y'):
-        print('joke')
+tellajoke = True
+
+while tellajoke:
+    ans = str(input('\n\nDo you want to read a joke?(y => yes; n => exit): '))
+    if ans == 'y':
+        x = requests.get('https://v2.jokeapi.dev/joke/Any')
+        x = x.json()
+
+        if 'joke' in x:
+            print('\n' + x['joke'])
+        else:
+            print('\n' + x['setup'])
+            print('...')
+            time.sleep(1)
+            print(x['delivery'])
     else:
-        telljoke = False
+        tellajoke = False
