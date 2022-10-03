2fb0ccbb6fe2b44b09c790c06fbceaee955ae6b8
kisb2 <kisb1024@gmail.com>
2022-10-03 17:51:30 +0200

    adding option for joke teller project to set joke category

--- Projects/Joke_teller/joketeller.py
+++ Projects/Joke_teller/joketeller.py
@@ -3,10 +3,12 @@ import time
 
 tellajoke = True
 
+category = "Any"
 while tellajoke:
-    ans = str(input('\n\nDo you want to read a joke?(y => yes; n => exit): '))
+    ans = str(input('\n\nDo you want to read a joke?(y => yes; n => exit; s=> settings): '))
+
     if ans == 'y':
-        x = requests.get('https://v2.jokeapi.dev/joke/Any')
+        x = requests.get('https://v2.jokeapi.dev/joke/' + category)
         x = x.json()
 
         if 'joke' in x:
@@ -16,5 +18,23 @@ while tellajoke:
             print('...')
             time.sleep(1)
             print(x['delivery'])
+    elif ans == 's':
+        s_ans = str(input('Which settings would you like to edit? (c => category): '))
+        if s_ans == "c":
+            c_ans = str(input('Selectable categories:\n a => Any\n p => Programming\n m => Misc\n d => dark\n s => '
+                              'Spooky\n c => Christmas\n'))
+            if c_ans == 'p':
+                category = 'Programming'
+            elif c_ans == "m":
+                category = "Misc"
+            elif c_ans == "d":
+                category = "Dark"
+            elif c_ans == "s":
+                category = "Spooky"
+            elif c_ans == "c":
+                category = "Christmas"
+            elif c_ans == "a":
+                category = "Any"
+            print("Category " + category + " set!")
     else:
         tellajoke = False
