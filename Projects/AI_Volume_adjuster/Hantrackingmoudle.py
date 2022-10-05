7325584a13d6ff840b9daacd274b2b4d41d6dfa2
HeyAnirudh <anirudh90hyd@gmail.com>
2022-10-05 17:29:36 +0530

    Ai-Volume-Adjuster

---
+++ Projects/AI_Volume_adjuster/Hantrackingmoudle.py
@@ -0,0 +1,90 @@
+import cv2
+import mediapipe as mp
+import time
+
+# class creation
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.modelComplex = modelComplexity
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils # it gives small dots onhands total 20 landmark points
+
+    def findHands(self,img,draw=True):
+        # Send rgb image to hands
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB) # process the frame
+    #     print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+
+                if draw:
+                    #Draw dots and connect them
+                    self.mpDraw.draw_landmarks(img,handLms,
+                                                self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self,img, handNo=0, draw=True):
+        """Lists the position/type of landmarks
+        we give in the list and in the list ww have stored
+        type and position of the landmarks.
+        List has all the lm position"""
+
+        lmlist = []
+
+        # check wether any landmark was detected
+        if self.results.multi_hand_landmarks:
+            #Which hand are we talking about
+            myHand = self.results.multi_hand_landmarks[handNo]
+            # Get id number and landmark information
+            for id, lm in enumerate(myHand.landmark):
+                # id will give id of landmark in exact index number
+                # height width and channel
+                h,w,c = img.shape
+                #find the position
+                cx,cy = int(lm.x*w), int(lm.y*h) #center
+                # print(id,cx,cy)
+                lmlist.append([id,cx,cy])
+
+                # Draw circle for 0th landmark
+                if draw:
+                    cv2.circle(img,(cx,cy), 15 , (255,0,255), cv2.FILLED)
+
+        return lmlist
+
+def main():
+    #Frame rates
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
+    while True:
+        success,img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+        cv2.imshow("Video",img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
