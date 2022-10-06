3876066f9ba40d87b896769bb29fae2ae6dc38db
arpitsengar99 <arpitsengar99@gmail.com>
2022-10-06 06:22:14 -0700

    Binary search algorithm python program

---
+++ Projects/Binary Search Algorithm.py
@@ -0,0 +1,19 @@
+def binarySearch(array, x, low, high):
+    if high >= low:
+        mid = low + (high - low)//2 
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return binarySearch(array, x, low, mid-1)
+        else:
+            return binarySearch(array, x, mid + 1, high)
+    else:
+        return -1
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = int(input("Enter a number between 1 and 10:"))
+result = binarySearch(array, x, 0, len(array)-1)
+if result != -1:
+    print("Element is present at position " + str(result))
+else:
+    print("Element not found")
