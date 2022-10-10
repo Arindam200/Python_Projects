b84a0fd74d34f8e7163e49c1a667f4aa55d31f45
thanmayee43 <thanmayeepvks@gmail.com>
2022-10-10 14:50:44 +0530

    Adding python code to find longest common prefix

---
+++ Leetcode_Solutions/Longest Common Prefix #14/solution.py
@@ -0,0 +1,23 @@
+def longestCommonPrefix(self, a):
+        size = len(a)
+  
+        # if size is 0, return empty string 
+        if (size == 0):
+            return ""
+    
+        if (size == 1):
+            return a[0]
+    
+        # sort the given strings
+        a.sort()
+        
+        # find the minimum length string
+        end = min(len(a[0]), len(a[size - 1]))
+    
+        # find the common prefix 
+        i = 0
+        while (i < end and a[0][i] == a[size - 1][i]):
+            i += 1
+    
+        ans = a[0][0: i]
+        return ans
