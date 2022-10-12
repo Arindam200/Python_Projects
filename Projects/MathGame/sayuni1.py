e5adcd7fb6c7e3208966193697b3d69acb8c71cd
Sayuni <115170428+sayusoori@users.noreply.github.com>
2022-10-12 17:55:00 +0530

    Convert "Zero" to 0 and "One" to 1
    I added a python file to convert "Zero" to 0 and "One" to 1

---
+++ Projects/MathGame/sayuni1.py
@@ -0,0 +1,11 @@
+##Convert "Zero" and "One" to "1" and "0"
+num=["Zero","One"]
+
+#then check key and print the 0 or 1
+for n in range (0,len(num)):
+    if num[n]=="Zero":
+        print("Zero =", 0)
+    elif num[n]=="One":
+        print("One =", 1)
+        
+   
