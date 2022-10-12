5f616964d41549f990e612dded2c335c57b0a12a
Sayuni <115170428+sayusoori@users.noreply.github.com>
2022-10-12 18:01:11 +0530

    Delete sayuni3.py

--- Projects/MathGame/sayuni3.py
+++
@@ -1,9 +0,0 @@
-#is the number even or odd
-
-while (True):
-    number=int(input("Enter the value: "))
-    if number%2==0:
-        print("Even number: ",number)
-    else:
-        print("Odd number: ",number)
-
