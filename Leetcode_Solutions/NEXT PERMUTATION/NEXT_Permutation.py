72dc919dd05b955b4d7cc4916cd1b6b753da702e
Mahiman jain <100436573+Mahi181@users.noreply.github.com>
2022-10-12 23:43:44 +0530

    Created a new file NEXT_Permutation.py
    Given an array of integers nums, find the next permutation of nums.

---
+++ Leetcode_Solutions/NEXT PERMUTATION/NEXT_Permutation.py
@@ -0,0 +1,35 @@
+# A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+# The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+
+
+
+class Solution(object):
+   def nextPermutation(self, nums):
+      found = False
+      i = len(nums)-2
+      while i >=0:
+         if nums[i] < nums[i+1]:
+            found =True
+            break
+         i-=1
+      if not found:
+         nums.sort()
+      else:
+         m = self.findMaxIndex(i+1,nums,nums[i])
+         nums[i],nums[m] = nums[m],nums[i]
+         nums[i+1:] = nums[i+1:][::-1]
+      return nums
+   def findMaxIndex(self,index,a,curr):
+      ans = -1
+      index = 0
+      for i in range(index,len(a)):
+         if a[i]>curr:
+            if ans == -1:
+               ans = curr
+               index = i
+            else:
+               ans = min(ans,a[i])
+               index = i
+      return index
+ob1 = Solution()
+print(ob1.nextPermutation([1,2,5,4,3]))
