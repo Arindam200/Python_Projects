c0a03e4e697f44caeb3f745b72735b1914a21eab
Sayuni <115170428+sayusoori@users.noreply.github.com>
2022-10-14 13:15:35 +0530

    Add a python file
    I added a python file to check whether the inserted number is ODD or EVEN

---
+++ Projects/MathGame/sayuni3.py
@@ -0,0 +1,9 @@
+#is the number even or odd
+
+while (True):
+    number=int(input("Enter the value: "))
+    if number%2==0:
+        print("Even number: ",number)
+    else:
+        print("Odd number: ",number)
+
