29593f9ea025df96828636b6b7ef923655f08b12
Jahnavi-5 <115690004+Jahnavi-5@users.noreply.github.com>
2022-10-14 22:43:08 +0530

    Leap Year calculation

---
+++ Projects/LeapYear.py
@@ -0,0 +1,10 @@
+year = int(input('enter year'))
+if year % 400 == 0:
+  print('it is a leap year')
+elif year % 4 == 0:
+  print('it is a leap year')
+elif year % 100 == 0:
+  print('not a leap year')
+else:
+  print('not a leap year')
+Prime Number Program 
