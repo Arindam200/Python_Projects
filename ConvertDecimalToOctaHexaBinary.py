420822cc4ffd7c7dca4237f21c5616011b66a005
Arindam Majumder <109217591+Arindam200@users.noreply.github.com>
2022-10-15 01:55:21 +0530

    Delete ConvertDecimalToOctaHexaBinary.py

--- ConvertDecimalToOctaHexaBinary.py
+++
@@ -1,5 +0,0 @@
- # Python Program to Convert Decimal to Binary, Octal, and Hexadecimal
-dec = int(input('Enter a positive number -> '))
-print(f"{dec} in binary is {bin(dec)}")
-print(f"{dec} in octal is {oct(dec)}")
-print(f"{dec} in hexadecimal is {hex(dec)}")
